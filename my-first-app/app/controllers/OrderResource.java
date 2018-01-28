42dc9786c16e9ebf50e93d8b3bb1be079c59d66e
kazboo <33283783+kazboo@users.noreply.github.com>
2018-01-29 00:56:07 +0900

    feat:オーダーリソースの修正

--- my-first-app/app/controllers/OrderResource.java
+++ my-first-app/app/controllers/OrderResource.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import controllers.bean.OrderForm;
 import play.data.Form;
 import play.mvc.Controller;
@@ -8,14 +11,16 @@ import views.html.order;
 
 public class OrderResource extends Controller {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderResource.class);
+
     public Result getOrder() {
+        LOGGER.info("accept GET /orders");
 
-        OrderForm orderForm = new OrderForm();
-        orderForm.setType("Research");
+        Form<OrderForm> orderForm = Form.form(OrderForm.class)
+                .fill(OrderForm.newInstance(request()));
 
-        Form<OrderForm> form = Form.form(OrderForm.class)
-                .fill(orderForm);
+        LOGGER.info("200 OK");
 
-        return ok(order.render("accept GET /orders", form));
+        return ok(order.render("accept GET /orders", orderForm));
     }
 }
--- my-first-app/app/controllers/bean/OrderForm.java
+++ my-first-app/app/controllers/bean/OrderForm.java
@@ -1,14 +1,50 @@
 package controllers.bean;
 
+import play.mvc.Http.Request;
+
 public class OrderForm {
 
-    private String _type;
+    private String _orderType;
 
-    public String getType() {
-        return _type;
+    private String _userId;
+
+    private String _userName;
+
+    private OrderForm(Request request) {
+        setOrderType(request.getQueryString("orderType"));
+        setUserId(request.getQueryString("userId"));
+        setUserName(request.getQueryString("userName"));
     }
 
-    public void setType(String type) {
-        _type = type;
+    public OrderForm() {
+
+    }
+
+    public static OrderForm newInstance(Request request) {
+        return new OrderForm(request);
+    }
+
+    public String getOrderType() {
+        return _orderType;
+    }
+
+    public void setOrderType(String orderType) {
+        _orderType = orderType;
+    }
+
+    public String getUserName() {
+        return _userName;
+    }
+
+    public void setUserName(String userName) {
+        _userName = userName;
+    }
+
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId(String userId) {
+        _userId = userId;
     }
 }
